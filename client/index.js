293c7849ba15dbe38bee9dbc4a38ac79c705ea4c
Linfei Fu <linfei.fu.2803@gmail.com>
2023-08-30 22:25:22 +1000

    created cards section

--- client/index.js
+++ client/index.js
@@ -1,13 +1,27 @@
 import "./index.css";
 
-import { highline, profile, cards } from "@/js/database";
+import { highline, profile, cards } from "@/js/database.js";
 
-import { left_profile_at, left_profile_name, left_profile_photo_img, create_news, create_picture_structure } from "@/js/common";
+import { 
+    left_profile_at, 
+    left_profile_name, 
+    left_profile_photo_img, 
+    create_news, 
+    create_picture_structure,
+    create_profile_structure,
+    create_control_structure,
+    create_comment_structure,
+ } from "@/js/common.js";
 
 left_profile_name.textContent = profile.name;
 left_profile_at.textContent = profile.at;
 left_profile_photo_img.src = profile.img_src;
 
+//nav
+const nav = document.querySelector("nav");
+const nav_profile_photo = nav.querySelector(".profile-photo");
+const nav_profile_photo_img = nav_profile_photo.querySelector("img");
+nav_profile_photo_img.src = `./images/profile/profile_1.png`
 
 
 //main middle
@@ -31,10 +45,16 @@ middle_highline_description_img.forEach((ele, i) => {
 })
 
 //middle form
-const middle_form = middle.querySelector("form")
-const middle_form_input_text = middle_form.querySelector('#create-post')
+const middle_form = middle.querySelector("form");
+const middle_form_profile_photo = middle_form.querySelector(".profile-photo");
+const middle_form_profile_photo_img = middle_form_profile_photo.querySelector("img");
+const middle_form_input_text = middle_form.querySelector('#create-post');
+
 //name 
 middle_form_input_text.placeholder = `What's on your mind, ${profile.name}`
+
+//profile-photo
+middle_form_profile_photo_img.src = `./images/profile/profile_1.png`
 
 //cards
 const cardsDom = document.querySelector(".cards");
@@ -42,5 +62,8 @@ const cardsDom = document.querySelector(".cards");
 cards.forEach((element) => {
     const doms = create_news();
     cardsDom.appendChild(doms.info);
-    create_picture_structure(doms.info_picture, element.picture)
+    create_picture_structure(doms.info_picture, element.picture);
+    create_profile_structure(doms.info_profile, element.profile);
+    create_control_structure(doms.info_control, element.control);
+    create_comment_structure(doms.info_comment, element.comment);
 })
--- client/src/js/common.js
+++ client/src/js/common.js
@@ -3,8 +3,8 @@ const main_container = main.querySelector(".container");
 
 //main left
 const left = main_container.querySelector(".main-left");
-// const left_profile = left.querySelector(".profile");
-const left_profile_photo = left.querySelector(".profile-photo");
+const left_profile = left.querySelector(".profile");
+const left_profile_photo = left_profile.querySelector(".profile-photo");
 export const left_profile_name = left.querySelector("h2");
 export const left_profile_at = left.querySelector("p");
 export const left_profile_photo_img = left_profile_photo.querySelector("img");
@@ -20,7 +20,7 @@ export const create_news = () => {
         info_comment: document.createElement("div"),
     }
     doms.info.classList.add("info");
-    doms.info_profile.classList.add("ifo-profile");
+    doms.info_profile.classList.add("info-profile");
     doms.info_picture.classList.add("info-picture");
     doms.info_control.classList.add("info-control");
     doms.info_comment.classList.add("info-comment");
@@ -39,3 +39,72 @@ export const create_picture_structure = (picture_dom, info) => {
     picture_dom.appendChild(picture);
     picture.src = info.img_src
 }
+
+export const create_profile_structure = (profile_dom, info) => {
+    const profile = document.createElement("div");
+    profile.classList.add("profile");
+    profile.innerHTML = `
+    <div class="profile-photo">
+        <img src="" alt="profile-photo">
+    </div>
+    <div class="profile-description">
+        <h2></h2>
+        <p></p>
+    </div>
+    `
+    const setting = document.createElement("i");
+    setting.className = "uil uil-ellipsis-h";
+    profile_dom.appendChild(profile);
+    profile_dom.appendChild(setting);
+
+    profile.querySelector("img").src = info.src;
+    profile.querySelector("h2").textContent = info.name;
+    profile.querySelector('p').textContent = `${info.position}, ${info.time}`;
+}
+
+// // Create Control
+export const create_control_structure = (control_dom) => {
+    const mainControl = document.createElement('div')
+    mainControl.classList.add('main-control')
+    const like = document.createElement('i')
+    like.className = "uil uil-heart control_heart"
+    const comment = document.createElement('i')
+    comment.className = "uil uil-comment-dots"
+    const share = document.createElement('i')
+    share.className = 'uil uil-share-alt'
+    mainControl.appendChild(like)
+    mainControl.appendChild(comment)
+    mainControl.appendChild(share)
+    const mark = document.createElement('i')
+    mark.className = 'uil uil-bookmark'
+    control_dom.appendChild(mainControl)
+    control_dom.appendChild(mark)
+  }
+  
+  // Create Comment
+  export const create_comment_structure = (comment_dom, info) => {
+    const likePanel = document.createElement('div')
+    likePanel.classList.add('profile-photo-list')
+    info.img_src_list.forEach(element => {
+      console.log(element);
+      const likePeople = document.createElement('img')
+      // likePeople.innerHTML = `<img alt = "profile photo">`
+      likePeople.src = element
+      likePeople.classList.add("profile-photo")
+      likePanel.appendChild(likePeople)
+    });
+  
+    const likeInfo = document.createElement('span')
+    likeInfo.classList.add('like-info')
+    likeInfo.innerHTML = `Liked by <strong> ${info.first_people_name}</strong > and <strong> ${info.like_peoples_number}</strong> others`
+    likePanel.appendChild(likeInfo)
+    const commentInfo = document.createElement('span')
+    commentInfo.classList.add('comment-info')
+    commentInfo.textContent = info.comment_info
+    const viewBtn = document.createElement('span')
+    viewBtn.classList.add('view-btn')
+    viewBtn.textContent = `View all ${info.view_number} comments`
+    comment_dom.appendChild(likePanel)
+    comment_dom.appendChild(commentInfo)
+    comment_dom.appendChild(viewBtn)
+  }
